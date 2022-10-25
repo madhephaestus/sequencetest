fa419375deca4ec46e77afae9a64d60faf2ebfa5
Kevin Harrington <kharrington@softroboticsinc.com>
2022-10-25 13:49:02 -0400

    adding mobile base loading

--- init.groovy
+++ init.groovy
@@ -1,2 +1,32 @@
-//Your code here
+
+MobileBase base=DeviceManager.getSpecificDevice( "JackSkellington",{
+	//If the device does not exist, prompt for the connection
+
+	MobileBase m = MobileBaseLoader.fromGit(
+			"https://github.com/Halloween2022RodPuppet/RodPuppetKinematics.git",
+			"rodpuppet.xml"
+			)
+	return m
+})
+
+MobileBase left=DeviceManager.getSpecificDevice( "JackSkellingtonLeftHand",{
+	//If the device does not exist, prompt for the connection
+
+	MobileBase m = MobileBaseLoader.fromGit(
+			"https://github.com/Halloween2022RodPuppet/RodPuppetKinematics.git",
+			"rodpuppet2.xml"
+			)
+	return m
+})
+
+MobileBase right=DeviceManager.getSpecificDevice( "JackSkellingtonRightHand",{
+	//If the device does not exist, prompt for the connection
+
+	MobileBase m = MobileBaseLoader.fromGit(
+			"https://github.com/Halloween2022RodPuppet/RodPuppetKinematics.git",
+			"rodpuppet3.xml"
+			)
+	return m
+})
+
 println "Initialize!"
